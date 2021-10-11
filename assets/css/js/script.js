9ad9f02387d1aef0c10b995f48c9a73929d51ddc
Sorin05 <sorinandrei230587@gmail.com>
2021-10-11 11:45:05 +0000

    Add constants to js

---
+++ assets/css/js/script.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const picks = Array.from(document.querySelectorAll('.pick-text'));
+const progressText = document.querySelector('#progressText');
+const progressBarFull = document.querySelector('#progressBarFull');
+const scoreText = document.querySelector ('#score');
