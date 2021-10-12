9976b5fa86a1d8b15acd570efb1d0efeee814fd7
Sorin05 <sorinandrei230587@gmail.com>
2021-10-12 10:54:55 +0000

    Add questions in js

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -3,3 +3,46 @@ const picks = Array.from(document.querySelectorAll('.pick-text'));
 const progressText = document.querySelector('#progressText');
 const progressBarFull = document.querySelector('#progressBarFull');
 const scoreText = document.querySelector ('#score');
+
+
+let currentQuestion= {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+
+let questions = [
+    {
+        question: 'What is 1 + 1?',
+        pick1: '2',
+        pick2: '3',
+        pick3: '23',
+        pick4: '5',
+        answer: 2,
+    },
+    {
+        question: 'What is 1 + 1?',
+        pick1: '2',
+        pick2: '3',
+        pick3: '23',
+        pick4: '5',
+        answer: 2,
+    },
+    {
+        question: 'What is 1 + 1?',
+        pick1: '2',
+        pick2: '3',
+        pick3: '23',
+        pick4: '5',
+        answer: 2,
+    },
+    {
+        question: 'What is 1 + 1?',
+        pick1: '2',
+        pick2: '3',
+        pick3: '23',
+        pick4: '5',
+        answer: 2,
+    }
+]
