b7eea248be9b90774d6597e114746d6c962d101f
Sorin05 <sorinandrei230587@gmail.com>
2021-10-18 18:39:20 +0000

    fix spelling mistake

--- assets/css/js/endpage.js
+++ assets/css/js/endpage.js
@@ -3,7 +3,7 @@ const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
 const mostRecentScore = document.querySelector('#mostRecentScore')
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScore')) || []
 
 const MAX_HIGH_SCORES = 5
 
